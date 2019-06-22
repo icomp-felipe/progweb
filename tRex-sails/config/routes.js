32bec73301cae4a8a2505ac600abb03eeed9d432
Felipe André <fass@icomp.ufam.edu.br>
2019-06-21 22:05:35 -0400

    Brincando com Modal

--- tRex-sails/config/routes.js
+++ tRex-sails/config/routes.js
@@ -34,7 +34,7 @@ module.exports.routes = {
   'GET /account':            { action: 'account/view-account-overview' },
   'GET /account/password':   { action: 'account/view-edit-password' },
   'GET /account/profile':    { action: 'account/view-edit-profile' },
-  'GET /account/curso':       { action: 'account/selecionar-curso' },
+  'GET /account/curso':      { action: 'account/selecionar-curso' },
   'POST /account/curso':     { action: 'account/selecionar-curso' },
 
   'GET /curso':                  { action: 'curso/index'  },
