e9157d802f5a4167096e8eec4a08f0b6ef31196c
Felipe André <fass@icomp.ufam.edu.br>
2019-07-02 00:42:20 -0400

    Correção de BUGs

--- tRex-sails/config/routes.js
+++ tRex-sails/config/routes.js
@@ -33,11 +33,11 @@ module.exports.routes = {
   'GET /password/forgot':    { action: 'entrance/view-forgot-password' },
   'GET /password/new':       { action: 'entrance/view-new-password' },
 
-  'GET /account':            { action: 'account/view-account-overview' },
-  'GET /account/password':   { action: 'account/view-edit-password' },
-  'GET /account/profile':    { action: 'account/view-edit-profile' },
-  'GET /account/curso':      { action: 'account/seleciona-curso' },
-  'POST /account/curso':     { action: 'account/seleciona-curso' },
+  'GET /account':                   { action: 'account/view-account-overview' },
+  'GET /account/password':          { action: 'account/view-edit-password' },
+  'GET /account/profile':           { action: 'account/view-edit-profile' },
+  'GET /account/seleciona-curso':   { action: 'account/seleciona-curso' },
+  'POST /account/seleciona-curso':  { action: 'account/seleciona-curso' },
 
   'GET /curso':                  { action: 'curso/index'  },
   'GET /curso/create':           { action: 'curso/create' },
